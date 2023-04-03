ea2093fad782614c7da16ef977ffd856ed7f4b7f
jamiuabdulkareem <jamiuabdulkareem08@gmail.com>
2023-04-03 03:19:28 +0100

    upload

--- app.js
+++ app.js
@@ -3,6 +3,8 @@
 const taskContainer = document.querySelector(".task");
 const taskInput = document.querySelector(".task__input");
 const taskBtn = document.querySelector(".task__btn");
+const btn = document.querySelector("#btnDele");
+const listItem = document.getElementById("list");
 
 let task = [];
 
@@ -12,8 +14,10 @@ const displayTask = function (display) {
   display.forEach(function (list) {
     const html = `<ul class="task__list">
     <li class="task__list--item">
-      <p>${list}</p>
+      <p id="list">${list}</p>
+      <button type="button" id="btnDele">
       <ion-icon class="delete__btn" name="trash-outline"></ion-icon>
+      </button>
     </li>
   </ul>`;
 
